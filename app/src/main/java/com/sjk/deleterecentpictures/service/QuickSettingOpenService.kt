22aabd29556a2277864460880fa5a52b84e3e508
来一斤BUG <1045290202@qq.com>
2023-09-18 20:52:24 +0800

    修复android 14无法从快捷面板启动的问题

--- app/src/main/java/com/sjk/deleterecentpictures/service/QuickSettingOpenService.kt
+++ app/src/main/java/com/sjk/deleterecentpictures/service/QuickSettingOpenService.kt
@@ -1,5 +1,7 @@
 package com.sjk.deleterecentpictures.service
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
 import android.service.quicksettings.TileService
@@ -10,21 +12,30 @@ import com.sjk.deleterecentpictures.activity.main.MainActivity
 class QuickSettingOpenService : TileService() {
     private var intent: Intent? = null
 
-    //当用户从Edit栏添加到快速设定中调用
+    // 当用户从Edit栏添加到快速设定中调用
     override fun onTileAdded() {}
 
-    //当用户从快速设定栏中移除的时候调用
+    // 当用户从快速设定栏中移除的时候调用
     override fun onTileRemoved() {}
 
     // 点击的时候
+    
+    @SuppressLint("StartActivityAndCollapseDeprecated")
     override fun onClick() {
-        intent = Intent(this, MainActivity::class.java)
-        intent!!.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivityAndCollapse(intent)
+        this.intent = Intent(this, MainActivity::class.java)
+        this.intent!!.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            this.startActivityAndCollapse(pendingIntent)
+        } else {
+            @Suppress("DEPRECATION")
+            this.startActivityAndCollapse(this.intent)
+        }
     }
 
     // 打开下拉菜单的时候调用,当快速设置按钮并没有在编辑栏拖到设置栏中不会调用
-    //在TleAdded之后会调用一次
+    // 在TleAdded之后会调用一次
     override fun onStartListening() {}
 
     // 关闭下拉菜单的时候调用,当快速设置按钮并没有在编辑栏拖到设置栏中不会调用
