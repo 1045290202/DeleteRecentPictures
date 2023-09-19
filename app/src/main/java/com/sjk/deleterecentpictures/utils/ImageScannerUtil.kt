6c344c8fd0a055aaf4e4b9de17e059c32d1e7000
来一斤BUG <1045290202@qq.com>
2023-09-19 10:18:39 +0800

    调整图片搜索代码

--- app/src/main/java/com/sjk/deleterecentpictures/utils/ImageScannerUtil.kt
+++ app/src/main/java/com/sjk/deleterecentpictures/utils/ImageScannerUtil.kt
@@ -12,39 +12,44 @@ import android.util.Log
 import com.sjk.deleterecentpictures.bean.ImageInfoBean
 import com.sjk.deleterecentpictures.common.DataSource
 import java.io.File
-import java.util.*
 
 
 object ImageScannerUtil {
     private const val TAG = "ImageScannerUtil"
-    var imagePaths: MutableList<String?>? = null
-    var imageUris: MutableList<Uri?>? = null
-
-    //    public var firstImageThumbnail: Bitmap? = null
+    
     private var cursor: Cursor? = null
-
-    const val DATE_MODIFIED = MediaStore.Images.Media.DATE_MODIFIED
-    const val DATE_ADDED = MediaStore.Images.Media.DATE_ADDED
+    
+    const val DATE_MODIFIED = MediaStore.Files.FileColumns.DATE_MODIFIED
+    const val DATE_ADDED = MediaStore.Files.FileColumns.DATE_ADDED
     
     val screenshotsPath: String
         get() {
             var path = "${Environment.getExternalStorageDirectory().absolutePath}/DCIM/Screenshots/"
             val file = File(path)
             if (!file.exists()) {
-                path = "${Environment.getExternalStorageDirectory().absolutePath}/Pictures/Screenshots/"
+                path =
+                    "${Environment.getExternalStorageDirectory().absolutePath}/Pictures/Screenshots/"
             }
             return path
         }
-
-    init {
-        imagePaths = ArrayList()
-        imageUris = ArrayList()
-    }
-
-    fun init(context: Context, selection: String?, escape: Boolean = true, sortOrder: String = this.DATE_MODIFIED) {
+    
+    fun init(
+        context: Context,
+        selection: String?,
+        escape: Boolean = true,
+        sortOrder: String = this.DATE_MODIFIED
+    ) {
         var realSelection = selection
         if (realSelection != null) {
-            realSelection = "${MediaStore.Images.Media.DATA} like '$selection%'${if (escape) " escape '\\'" else ""}"
+            realSelection = """
+                ${MediaStore.Files.FileColumns.DATA} like '$selection%'${
+                if (escape) {
+                    " escape '\\'"
+                } else {
+                    ""
+                }
+            }
+            """.trimIndent()
         }
         
         this.cursor = this.getQuery(context, realSelection, sortOrder)
@@ -53,32 +58,44 @@ object ImageScannerUtil {
     
     private fun getQuery(context: Context, realSelection: String?, sortOrder: String): Cursor? {
         return context.contentResolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null, realSelection, null,
-                "$sortOrder DESC"
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            arrayOf(
+                MediaStore.MediaColumns._ID,
+                MediaStore.MediaColumns.DATA,
+                // MediaStore.MediaColumns.DATE_ADDED,
+                // MediaStore.MediaColumns.DATE_MODIFIED,
+                // MediaStore.MediaColumns.DISPLAY_NAME,
+                // MediaStore.MediaColumns.MIME_TYPE
+            ),
+            realSelection,
+            null, // selectionArgs,
+            "$sortOrder DESC",
         )
     }
-
+    
     fun getCurrent(): ImageInfoBean? {
         var imageInfo: ImageInfoBean? = null
         cursor?.let {
             imageInfo = try {
-                val columnIndexData: Int = it.getColumnIndex(MediaStore.Images.Media.DATA)
+                val columnIndexData: Int = it.getColumnIndex(MediaStore.MediaColumns.DATA)
                 val imagePath = it.getString(columnIndexData)
-
-                val columnIndexId: Int = it.getColumnIndex(MediaStore.Images.Media._ID)
+                
+                val columnIndexId: Int = it.getColumnIndex(MediaStore.MediaColumns._ID)
                 val imageId: Long = it.getLong(columnIndexId)
-                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageId)
-
+                val imageUri = ContentUris.withAppendedId(
+                    MediaStore.Files.getContentUri("external"),
+                    imageId,
+                )
+                
                 ImageInfoBean(imagePath, imageUri, imageId)
             } catch (e: CursorIndexOutOfBoundsException) {
                 null
             }
         }
-
+        
         return imageInfo
     }
-
+    
     fun getNext(): ImageInfoBean? {
         cursor?.let {
             if (it.isLast) {
@@ -88,7 +105,7 @@ object ImageScannerUtil {
         }
         return getCurrent()
     }
-
+    
     fun getPrevious(): ImageInfoBean? {
         cursor?.let {
             if (it.isFirst) {
@@ -98,7 +115,7 @@ object ImageScannerUtil {
         }
         return getCurrent()
     }
-
+    
     fun isEnd(): Boolean {
         cursor?.let {
             return it.isLast
@@ -109,74 +126,5 @@ object ImageScannerUtil {
     fun close() {
         this.cursor?.close()
         this.cursor = null
-    }
-    
-    /**
-     * 获取媒体扫描图片路径
-     */
-    fun searchImages(context: Context, selection: String?, escape: Boolean) {
-        var sel = selection
-        if (sel != null) {
-            sel = "${MediaStore.Images.Media.DATA} like '$sel%'${if (escape) " escape '\\'" else ""}"
-        }
-        Log.d(TAG, "getImages: $sel")
-        try {
-            val numberOfPictures: Int = DataSource.getNumberOfPictures()
-            
-            val cursor = context.contentResolver
-                    .query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                            null,
-                            sel,
-                            null,
-                            "${MediaStore.Images.Media.DATE_MODIFIED} DESC") //MediaStore.Images.Media._ID + " DESC" //按照修改日期排序
-            if (cursor != null && cursor.moveToFirst()) {
-                var i: Long = 0
-                do {
-                    val imageId: Long = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID))
-                    val displayName: String =
-                            cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
-                    val imageUri: Uri =
-                            ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageId)
-
-                    val columnIndex: Int = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
-                    val imagePath = cursor.getString(columnIndex)
-                    imagePaths!!.add(imagePath)
-                    i++
-                    if (i > numberOfPictures) {
-                        break
-                    }
-                } while (cursor.moveToNext())
-                cursor.close()
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-//        Log.d(TAG, "getImages: $imagePaths");
-    }
-    
-    fun refreshMediaLibraryByPath(context: Context, filePath: String?, type: Int, cb: (path: String, uri: Uri?) -> Unit) {
-        if (filePath == null) {
-            return
-        }
-        
-        when (type) {
-            1 -> {
-                MediaScannerConnection.scanFile(context, arrayOf(filePath), null) { path: String, uri: Uri? ->
-                    cb(path, uri)
-                    Log.d(TAG, "媒体库更新成功！")
-                }
-            }
-            2 -> {
-                val where = "${MediaStore.Audio.Media.DATA} like \"$filePath%\""
-                val i = context.contentResolver.delete(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, where, null)
-                if (i > 0) {
-                    Log.d(TAG, "媒体库更新成功！")
-                }
-                cb(filePath, null)
-            }
-            else -> {
-                Log.e(TAG, "查询类型错误")
-            }
-        }
     }
 }
